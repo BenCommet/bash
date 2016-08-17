a4189891061f0c8387a60b42c54a6a58a1baa2b1
Ben Commet <benc@schoolzone.com>
2016-08-17 17:04:23 -0400

    new version of script

--- gamePass.py
+++ gamePass.py
@@ -3,6 +3,7 @@ import sys
 import os
 from distutils.dir_util import copy_tree
 import fileinput
+import time
 #make a new directory if the new Directory isn't yet made
 newDirPath = os.getcwd() +'/new'
 mainAssets = newDirPath + '/assets'
@@ -29,27 +30,46 @@ for i in directoryFiles:
 		#make new versions in the main-js directory to hold the javascript
 		newJsDir = mainJs + '/' + i
 		os.makedirs(newJsDir)
+
 		#make new versions in the main-assets directory to hold the asset folders
 		newAssetsDir = mainAssets + '/' + i
 		os.makedirs(newAssetsDir);
-		oldVersionFiles = os.listdir(i)
+
+		#get the asset path of the old directories
 		assetPath = currentDirectory + '/' + i +'/assets'
 		jsPath =  currentDirectory + '/' + i +'/js'
+		
+		#copy the files to their new home
 		copy_tree(jsPath, newJsDir)
 		copy_tree(assetPath, newAssetsDir)
 
+		#used to hold the assetSize
+		assetSize = ''
+		orientation = ''
+
+		#we have two for loops for boot because we need data that is held in the bottom of the file at the top
 		for line in fileinput.input(newJsDir + '/Boot.js', inplace = True):
 			print line.rstrip().replace(i +'/assets', 'assets/' + i)
+			if 'szPhaserPreload' in line:
+				assetsize = line[17:22]
+			if 'szPhaserPreload' in line and 'true' in line:
+				orientation = 'true'
+			else:
+				orientation = 'false'
 
 		for line in fileinput.input(newJsDir + '/Boot.js', inplace = True):
-				if 'var assetRoot =' in line:
-					print line.rstrip().replace('var assetRoot =', 'var cdn = szPhaserCDN();\n\nvar assetRoot =')
-					print 'var activityData = {'
-					print '"title" : "' + title + '",'
-					print '"version" : "' + i + '",'
-					print '}'
-				else:
-					print line.rstrip()
+
+			if 'var assetRoot =' in line:
+				print line.rstrip().replace('var assetRoot =', 'var cdn = szPhaserCDN();\n\nvar assetRoot =')
+				print 'var activityData = {'
+				print '"assetSize" : "' + assetsize + '",'
+				print '"forceOrientation" : "' + orientation + '",'
+				print '"title" : "' + title + '",'
+				print '"version" : "' + i + '",'
+				print '"date" : "' + time.strftime("%m/%d/%y") +'",'
+				print '}'
+			else:
+				print line.rstrip()
 		
 
 		for line in fileinput.input(newJsDir + '/Game.js', inplace = True):
@@ -58,5 +78,4 @@ for i in directoryFiles:
 			noHelpButton = 'help' not in line or 'Selected:' in line or 'play(' in line
 			if noMusicButton and noExitButton and noHelpButton:
 				print line.rstrip()
-
 
